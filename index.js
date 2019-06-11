4025e2f064f686539ebff0751b7bdd02a2a52961
Jaco Malan <jacomalan.12@gmail.com>
2019-06-11 16:27:30 +0200

    Switched to using FireBase RealTime DB

--- index.js
+++ index.js
@@ -1,50 +1,29 @@
 const express = require('express');
-const mysql = require('mysql');
 const fs = require('fs');
 const User = require('./user.js');
+const firebase = require('firebase/app');
+require('firebase/firestore');
 
-// Load the AuthDB credentials from a JSON file.
-const sqlCreds = JSON.parse(fs.readFileSync('creds.json'));
+// Get FireBase creds
+const fbCreds = JSON.parse(fs.readFileSync('./creds.json'));
 
-// Add the required SSL certificates.
-sqlCreds.ssl = {
-    ca: fs.readFileSync('server-ca.pem'),
-    key: fs.readFileSync('client-key.pem'),
-    cert: fs.readFileSync('client-cert.pem')
-};
+firebase.initializeApp(fbCreds);
+const fbDatabase = firebase.database();
+const dbRef = fbDatabase.ref('users');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
 
-app.post('/login', (req, res) => {
+const testUser = {
 
-    const params = req.body;
+    uName: 'test',
+    uHash: 'FFAAE23331A'
 
-    if (!params.uname || !params.pword) {
+};
 
-        res.json({ status: "error", error: "Invalid username or password." }).end();
-
-    } else {
-
-        const user = new User(params.uname, sqlCreds);
-        user.getProps();
-
-        user.authenticate(params)
-            .then(result => {
-
-                if (result.logged === false && result.status === 'error') {
-                    res.json({ status: 'error', error: result.error });
-                } else {
-                    res.json({ status: 'success', token: result.token });
-                }
-
-            });
-
-    }
-
-});
+dbRef.push(testUser).then(status => console.log('Success!')).catch(err => console.log(err));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
