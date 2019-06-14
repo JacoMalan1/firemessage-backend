1da36337241eb5ca23ed402c5d21d61a42ea23bc
Jaco Malan <jacomalan.12@gmail.com>
2019-06-14 16:22:37 +0200

    Update

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
 const express = require('express');
+const mysql = require('mysql');
 const fs = require('fs');
 const User = require('./user.js');
-const firebase = require('firebase/app');
-require('firebase/firestore');
 
-// Get FireBase creds
-const fbCreds = JSON.parse(fs.readFileSync('./creds.json'));
+// Load the AuthDB credentials from a JSON file.
+const sqlCreds = JSON.parse(fs.readFileSync('./creds.json'));
 
-firebase.initializeApp(fbCreds);
-const fbDatabase = firebase.database();
-const dbRef = fbDatabase.ref('users');
+// Add the required SSL certificates.
+// sqlCreds.ssl = {
+//     ca: fs.readFileSync('server-ca.pem'),
+//     key: fs.readFileSync('client-key.pem'),
+//     cert: fs.readFileSync('client-cert.pem')
+// };
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
 
-const testUser = {
+app.post('/login', (req, res) => {
 
-    uName: 'test',
-    uHash: 'FFAAE23331A'
+    const params = req.body;
 
-};
+    if (!params.uname || !params.pword) {
 
-dbRef.push(testUser).then(status => console.log('Success!')).catch(err => console.log(err));
+        res.json({ status: "error", error: "Invalid username or password." }).end();
+
+    } else {
+
+        const user = new User(params.uname, sqlCreds);
+        user.getProps();
+
+        user.authenticate(params)
+            .then(result => {
+
+                if (result.logged === false && result.status === 'error') {
+                    res.json({ status: 'error', error: result.error });
+                } else {
+                    res.json({ status: 'success', token: result.token });
+                }
+
+            });
+
+    }
+
+});
+
+const u = new User('jacom', sqlCreds);
+u.getProps()
+    .then(props => {
+        console.log('success');
+        console.log(u);
+    })
+    .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- user.js
+++ user.js
@@ -1,14 +1,127 @@
+const mysql = require('mysql');
+const Sql = require('./sql.js');
+const fs = require('fs');
+
 class User {
 
-    constructor(name) {
+    /**
+     * Constructs a new User object.
+     * @param {string} uname Username
+     * @param {Object} dbCreds Object containing the SQL credentials
+     */
+    constructor(uname, dbCreds) {
 
-        this.name = name;
+        this.name = uname;
+        this.hash = null;
+        this.dbCreds = dbCreds;
+        this.type = null;
+        this.id = -1;
+        this.gotProps = false;
 
     }
 
-    async getProps() { }
+    /**
+     * Gets the properties of the user and stores them
+     * in the current User object.
+     */
+    async getProps() {
 
-    async authenticate() { }
+        const sql = new Sql(this.dbCreds);
+        await sql.connect();
+
+        const query = await sql.query('SELECT * FROM fmdb.tblUsers WHERE uName = ' + mysql.escape(this.name));
+
+        if (query.results.length != 1) {
+            return;
+        }
+
+        this.hash = query.results[0].uHash;
+        this.id = query.results[0].uID;
+        this.getProps = true;
+
+    }
+
+    /**
+     * Authenticates the user and returns an
+     * object with the auth status.
+     */
+    async authenticate(creds) {
+
+        const auth = { logged: false, status: 'error', error: '', token: null };
+
+        if (!this.getProps) {
+            auth.error = 'You have to run getProps() first.';
+            return auth;
+        }
+
+        if (creds.uname != this.name) {
+            auth.error = 'Wrong user object.';
+            return auth;
+        }
+
+        const sql = new Sql(this.dbCreds);
+        await sql.connect();
+
+        const query = await sql.query('SELECT * FROM fmdb.tblUsers WHERE uName = ' + mysql.escape(this.name));
+
+        if (query.results.length != 1) {
+            auth.error = 'No such user.';
+            return auth;
+        }
+
+        const tHash = this.computeHash(creds.pword);
+        if (tHash = this.hash) {
+            auth.logged = true;
+            auth.status = 'success';
+            auth.token = 'Generic token';
+        } else {
+            auth.error = 'Wrong password.';
+        }
+
+        return auth;
+
+    }
+
+    /**
+     * Saves the user to the database.
+     */
+    async save() {
+
+        const sql = new Sql(this.dbCreds);
+        await sql.connect();
+
+        let query = await sql.query('SELECT uID FROM fmdb.tblUsers WHERE uName = ' + mysql.escape(this.name));
+        if (query.results.length > 0 && query.results.uID == this.id) {
+            throw new Error("This user already exists.");
+        } else if (query.results.length == 1 && query.results.uName == this.name) {
+
+            const eHash = mysql.escape(this.hash);
+
+            sql.query(`UPDATE fmdb.tblUsers SET uHash = ${eHash} WHERE uID = ${String(this.id)};`)
+                .catch(err => { throw err; });
+
+        } else {
+
+            const eName = mysql.escape(this.name);
+            const eHash = mysql.escape(this.hash);
+            await sql.query(`INSERT INTO fmdb.tblUsers (uName, uHash) VALUES (${eName}, ${eHash});`)
+                .catch(err => { throw err; });
+
+        }
+
+        sql.close();
+
+    }
+
+    /**
+     * Computes and returns the hash for the
+     * given password.
+     * @param {string} pass String to hash
+     * @returns {string} Returns a hex string
+     */
+    computeHash(pass) {
+        return pass;
+    }
 
 }
 
